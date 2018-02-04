f96e18f33b9bdbfcb097b253f1b6069596e1f6cf
dioib <dioib@hotmail.co.jp>
2018-02-04 09:34:37 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: __dirname + "/src/index.js",
+    output: {
+      path: __dirname +'/dist', //ビルドしたファイルを吐き出す場所
+      filename: 'bundle.js' //ビルドした後のファイル名
+    },
+    module: {
+      loaders: [
+      {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query://loaderに渡したいクエリパラメータを指定します
+          {
+            presets: ['es2015','stage-0','react']
+          }
+      }]
+    }
+  };
